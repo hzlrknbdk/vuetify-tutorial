f955fa29208edfe3e549749eda9336e4c69b6efd
Hazal Ruken Budak <hazal.budak@pusulayazilim.com>
2020-08-04 15:00:32 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
+import NotFound from "./views/NotFound";
 
-Vue.config.productionTip = false
+import "./scss/main.scss";
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+    { path: "/contact", component: Contact },
+    { path: "*", component: NotFound },
+  ],
+  mode: "hash",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
